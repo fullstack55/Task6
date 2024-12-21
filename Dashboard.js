7eb0923a549f2f626e11d9caf3dc0158efdebaad
fullstack55 <jyothiomsaimanasa256@gmail.com>
2024-12-21 19:42:09 +0530

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,15 @@
+Dashboard.js
+import React from 'react';
+import './styles.css';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <h1>Welcome to the Dashboard</h1>
+            <p>Capturing The Beauty of the Nature</p>
+            
+        </div>
+    );
+};
+
+export default Dashboard;
